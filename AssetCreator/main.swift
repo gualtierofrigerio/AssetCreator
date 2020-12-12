a5184bc0a430a73a84efd63b5ecd7ee9b38f0048
Gualtiero Frigerio <gualtiero.frigerio@gmail.com>
2020-12-12 10:37:14 +0100

    added configurationPath

--- AssetCreator/main.swift
+++ AssetCreator/main.swift
@@ -11,9 +11,10 @@ import Foundation
 struct AssetCreator:ParsableCommand {
     @Argument var imagePath:String
     @Argument var outputPath:String
+    @Option var configurationPath:String?
     
     func run() {
-        let helper = AssetCreatorHelper()
+        let helper = AssetCreatorHelper(withConfigurationAtPath: configurationPath)
         let result = helper.createAssetFromImage(atPath: imagePath, outputPath: outputPath)
         if result == false {
             print("Error while creating asset images")
